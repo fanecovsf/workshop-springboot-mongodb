0fbe69a05aa23ad2137e2637528f7d0560b36000
fanecovsf <gustavo_h07@hotmail.com>
2023-08-14 06:32:44 -0300

    🆕 Feat:
    Obtendo usuário por ID e tratamento de erro 404

--- src/main/java/com/mongodb/workshop/services/UserService.java
+++ src/main/java/com/mongodb/workshop/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mongodb.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.workshop.domain.User;
 import com.mongodb.workshop.repositories.UserRepository;
+import com.mongodb.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }

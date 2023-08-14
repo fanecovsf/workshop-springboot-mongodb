df0dced1f5c1cb7b24c11984152ef63ce63b6e82
fanecovsf <gustavo_h07@hotmail.com>
2023-08-14 06:04:25 -0300

    🆕 Feat:
    Inserido configurações para insert automático de teste

---
+++ src/main/java/com/mongodb/workshop/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.mongodb.workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.workshop.domain.User;
+import com.mongodb.workshop.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
